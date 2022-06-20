3663032f4acc47ef288f968029f04ea05a600d75
Tomas Ek <tonken.ek@gmail.com>
2022-06-20 12:04:13 +0100

    started email service

---
+++ services/emailService.js
@@ -0,0 +1,25 @@
+const sgMail = require("@sendgrid/mail");
+
+require("dotenv").config;
+
+const OrderConfirmed = (orderInfo) => {
+  sgMail.setApiKey(process.env.SENDGRID_KEY);
+  const msg = {
+    to: `${orderInfo.eMail}`, // Change to your recipient
+    from: "tomas.ek@utb.ecutbildning.se", // Change to your verified sender
+    subject: "Order confirmed",
+    text: "Order is confirmed and waiting to be handled",
+    html: `<h1>Hi ${orderInfo.firstname}, your order is confirmed and waiting to be handled.</h1>
+    <p> Thanks for shopping at our store. You will be updated as soon as your order is processed.</p> `,
+  };
+  sgMail
+    .send(msg)
+    .then(() => {
+      console.log("Email sent");
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+};
+
+module.exports = { OrderConfirmed };
